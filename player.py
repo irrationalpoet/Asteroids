d06fab351fe2905bcaa5073871fb8ba4408072bd
irrationalpoet <irrationalpoet@gmail.com>
2026-07-03 04:21:02 -0400

    Added timer to limit shots

--- player.py
+++ player.py
@@ -3,14 +3,15 @@ import pygame
 from pygame import Vector2, Surface
 import circleshape
 from shot import Shot
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED 
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SHOOT_COOLDOWN_SECONDS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED 
 
 class Player(circleshape.CircleShape):
     position: Vector2
 
-    def __init__(self, x: float, y: float) -> None:
+    def __init__(self, x: float, y: float, shot_timer=0.0) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation: float = 0
+        self.rotation = 0.0
+        self.shot_timer = shot_timer
 
     def triangle(self) -> list[Vector2]:
         # Calculates the three vertices of the player's triangle based on current rotation.
@@ -32,8 +33,9 @@ class Player(circleshape.CircleShape):
 
     @override
     def update(self, dt: float) -> None:
+        self.shot_timer -= dt
+
         keys =  pygame.key.get_pressed()
-        
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -51,5 +53,9 @@ class Player(circleshape.CircleShape):
         self.position += rotated_vector * PLAYER_SPEED * dt
 
     def shoot(self) -> None:
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.shot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+        else:
+            return
