ebcdf2dec6958dc5d5d97a3714bfa083baeffe73
irrationalpoet <irrationalpoet@gmail.com>
2026-07-03 04:32:02 -0400

    Added kill logic for shots hitting asteroids

--- main.py
+++ main.py
@@ -40,6 +40,13 @@ def main() -> None:
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
          
         screen.fill("black")
         for obj in drawable:
