b9cd5ad55c5c3fc0ef6f084cd26c05b7f74219c4
irrationalpoet <irrationalpoet@gmail.com>
2026-07-03 04:56:51 -0400

    Updated asteroid behavior with split() method

--- asteroid.py
+++ asteroid.py
@@ -2,7 +2,9 @@ from typing import override
 import pygame
 from pygame import Surface
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -15,3 +17,21 @@ class Asteroid(CircleShape):
     @override
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid split")
+        new_asteroids_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_asteroids_radius) # type: ignore
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_asteroids_radius) # type: ignore
+       
+        rand_angle = random.uniform(20, 50)
+
+        asteroid_1.velocity = self.velocity.rotate(rand_angle)
+        asteroid_2.velocity = self.velocity.rotate(-rand_angle)
+
+
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main() -> None:
             for shot in shots:
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
          
         screen.fill("black")
